c59720f691be17dcc7d7c3156b6b68f2a91f00a4
Jasper900807 <121182794+Jasper900807@users.noreply.github.com>
2023-12-25 23:19:13 +0800

    Add files via upload

--- Lagrange.py
+++ Lagrange.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-
 def lagrange_interpolation(x, y, target_x):
     result = 0
     for i in range(len(y)):
@@ -14,7 +13,7 @@ def lagrange_interpolation(x, y, target_x):
     return result
 
 def enlarge_image_lagrange_xy(original_image, scale_factor):
-    # 獲取圖片的高度和寬度
+    # 取得影像的高度和寬度
     height, width = original_image.shape
 
     # 先對 x 軸進行拉格朗日插值
@@ -31,33 +30,38 @@ def enlarge_image_lagrange_xy(original_image, scale_factor):
             y = i / scale_factor
             y_enlarged[i, j] = lagrange_interpolation(np.arange(int(width * scale_factor)), x_enlarged[:, j], y)
 
-    return y_enlarged
+    # 確保灰階值在0到255之間
+    enlarged_image = np.clip(y_enlarged, 0, 255).astype(np.uint8)
 
-# 讀取圖片
-image_path = "/project/NA_proposal/image/img7.png"
+    return enlarged_image
+
+# 讀取影像
+#image_path = "/project/NA_proposal/image/img44.png"
 #original_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# or繪製簡單圖片
-original_image = np.array([[1,2,3],
-                           [4,5,6],
-                           [7,8,9]
+# or繪製簡單影像
+original_image = np.array([[255,255,255,255,255],
+                           [0,255,255,255,0],
+                           [0,255,0,255,0],
+                           [255,0,255,0,255],
+                           [255,255,255,255,255]
                            ])
 
-# 設定放大倍數
-scale_factor = 5
+# 設定放大倍率
+scale_factor = 2
 
-# 進行圖片放大
+# 進行影像放大
 enlarged_image = enlarge_image_lagrange_xy(original_image, scale_factor)
 
-# 繪製原始圖片
+# 繪製原始影像
 plt.subplot(1, 2, 1)
 plt.imshow(original_image, cmap='gray')
 plt.title('Original Image')
 
-# 繪製放大後的圖片
+# 繪製放大後的影像
 plt.subplot(1, 2, 2)
 plt.imshow(enlarged_image, cmap='gray')
 plt.title('Enlarged Image')
 
-# 顯示圖片
+# 顯示影像
 plt.show()
